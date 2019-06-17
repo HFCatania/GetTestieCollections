6bd170a31a98494b2f64ddfdd7aed49c8ebef124
Henry <cataniahf@gmail.com>
2019-06-16 20:03:10 -0400

    More tests done

--- gettestie/src/test/java/rocks/zipcode/HashMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -33,4 +33,57 @@ public class HashMapTest {
 
         Assert.assertTrue(testMap.containsKey(person1));
     }
+
+    @Test
+    public void hashMapTest3(){
+        HashMap<Person, Address> testMap = new HashMap<>();
+        testMap.put(person1,address1);
+        testMap.put(person2, address2);
+        testMap.put(person3, address3);
+
+        testMap.clear();
+
+        Assert.assertTrue(testMap.isEmpty());
+    }
+
+    @Test
+    public void hashMapTest4(){
+        HashMap<Person, Address> testMap = new HashMap<>();
+        HashMap<Person, Address> testMap2 = new HashMap<>();
+        testMap.put(person3, address3);
+        testMap.put(person1, address1);
+        testMap.put(person2, address2);
+
+        Integer expected = 3;
+        Integer actual = testMap.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hashMapTest5(){
+        HashMap<Person, Address> testMap = new HashMap<>();
+        testMap.put(person2, address2);
+
+        Assert.assertTrue(testMap.remove(person2, address2));
+    }
+
+    @Test
+    public void hashMapTest6(){
+        HashMap<Person, Address> testMap = new HashMap<>();
+        testMap.put(person3, address1);
+
+        Assert.assertFalse(testMap.remove(person3,address2));
+    }
+
+    @Test
+    public void hashMapTest7(){
+        HashMap<Person, Address> testMap = new HashMap<>();
+        testMap.put(person2, address2);
+        testMap.put(person1, address3);
+        testMap.put(person3, address1);
+
+        Assert.assertNotNull(testMap.values());
+
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/HashSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -1,4 +1,55 @@
 package rocks.zipcode;
 
+import gettestie.src.main.java.rocks.zipcode.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class HashSetTest {
+    Person person1 = new Person("MJ", 1950);
+    Person person2 = new Person("JamesBrown", 1930);
+    Person person3 = new Person("SamCooke", 1930);
+
+    @Test
+    public void hashSetTest1(){   //add
+        HashSet<Person> testSet = new HashSet<>();
+
+        Assert.assertTrue(testSet.add(person1));
+    }
+
+    @Test
+    public void hashSetTest2(){  //clear and is empty
+        HashSet<Person> testSet = new HashSet<>();
+        testSet.add(person1);
+        testSet.add(person2);
+        testSet.add(person3);
+
+        testSet.clear();
+
+        Assert.assertTrue(testSet.isEmpty());
+    }
+
+    @Test
+    public void hashSetTest3(){ //contains
+        HashSet<Person> testSet = new HashSet<>();
+        testSet.add(person2);
+
+        Assert.assertTrue(testSet.contains(person2));
+    }
+
+    @Test
+    public void hashSetTest4(){ // size
+        HashSet<Person> testSet = new HashSet<>();
+        testSet.add(person2);
+        testSet.add(person1);
+        testSet.add(person3);
+
+        Integer expected = 3;
+        Integer actual = testSet.size();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
